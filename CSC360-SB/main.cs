bca425eddc4ac1310ab5182bb7696301122312f0
lstevensN <lstevens@student.neumont.edu>
2025-05-25 15:17:18 -0600

    Added main.cs

---
+++ CSC360-SB/main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(" -- Start Program -- ");
+
+        Console.WriteLine("\n"); // Do stuff here!
+
+        Console.WriteLine(" --  End Program  -- \n");
+    }
+}
