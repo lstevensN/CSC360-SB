4069b4cf48d5af6fcc004aa85e6f64998d60175a
lstevensN <lstevens@student.neumont.edu>
2025-05-29 08:23:40 -0600

    Added Structural Design Pattern

--- CSC360-SB/main.cs
+++ CSC360-SB/main.cs
@@ -9,13 +9,23 @@ namespace CSC360_SB
             Console.WriteLine(" -- Start Program -- \n");
 
             // -- CREATIONAL Design Pattern --
-            Console.WriteLine(">> CREATIONAL Design Pattern");
+            Console.WriteLine(">> CREATIONAL Design Pattern  (Singleton)");
 
             Singleton singleton1 = Singleton.GetInstance();
             Singleton singleton2 = Singleton.GetInstance();
 
-            if (singleton1 == singleton2) Console.WriteLine("Singleton implementation succeeded!\n");
-            else Console.WriteLine("Singleton implementation failed.\n");
+            Console.WriteLine("> Singleton #1 = Singleton #2: " + (singleton1 == singleton2).ToString());
+
+
+            // -- STRUCTURAL Design Pattern
+            Console.WriteLine("\n>> STRUCTURAL Design Pattern  (Facade)");
+
+            SubsystemOne sub1 = new SubsystemOne();
+            SubsystemTwo sub2 = new SubsystemTwo();
+
+            Facade facade = new Facade(sub1, sub2);
+
+            Client.ClientCode(facade);
 
             Console.WriteLine(" --  End Program  -- \n");
         }
---
+++ CSC360-SB/structural.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC360_SB
+{
+    // Facade
+    public class Facade
+    {
+        private SubsystemOne subsystem1;
+
+        private SubsystemTwo subsystem2;
+
+
+        public Facade(SubsystemOne subsystem1, SubsystemTwo subsystem2)
+        {
+            this.subsystem1 = subsystem1;
+            this.subsystem2 = subsystem2;
+        }
+
+        public string Operation()
+        {
+            string result = "> Facade: Start Subsystems:\n";
+            result += subsystem1.Start() + "\n";
+            result += subsystem2.Init() + "\n";
+
+            result += "> Facade: Perform Subsystem Actions:\n";
+            result += subsystem1.DoAction() + "\n";
+            result += subsystem2.Engage() + "\n";
+
+            return result;
+        }
+    }
+
+
+    // Subsystem Class #1
+    public class SubsystemOne
+    {
+        public string Start()
+        {
+            return "SubsystemOne: Ready!";
+        }
+
+        public string DoAction()
+        {
+            return "SubsystemOne: 1 + 1 = " + (1 + 1).ToString();
+        }
+    }
+
+
+    // Subsystem Class #2
+    public class SubsystemTwo
+    {
+        public string Init()
+        {
+            return "SubsystemTwo: Initialized!";
+        }
+
+        public string Engage()
+        {
+            return "SubsystemTwo: ENGAGED!!!";
+        }
+    }
+
+
+    // Client
+    public class Client
+    {
+        public static void ClientCode(Facade facade)
+        {
+            Console.WriteLine(facade.Operation());
+        }
+    }
+}
