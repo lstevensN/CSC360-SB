78cec0cdfda130666818ebae767abfc3ab76ce87
lstevensN <lstevens@student.neumont.edu>
2025-05-29 08:46:08 -0600

    Added Behavioral Design Pattern

---
+++ CSC360-SB/behavioral.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSC360_SB
+{
+    // Iterator Interface
+    public abstract class Iterator<T>
+    {
+        public abstract bool HasNext();
+
+        public abstract T Next();
+    }
+
+
+    // Concrete Iterator
+    public class CollectionIterator<T> : Iterator<T>
+    {
+        private Collection<T> collection;
+
+        private int index = 0;
+
+
+        public CollectionIterator(Collection<T> collection) { this.collection = collection; }
+
+        public override bool HasNext() { return index < collection.Size(); }
+
+        public override T Next()
+        {
+            if (HasNext()) return collection.Get(index++);
+            else return default(T);
+        }
+    }
+
+
+    // Aggregate Interface
+    public abstract class IteratorAggregate<T>
+    {
+        public abstract Iterator<T> CreateIterator();
+    }
+
+
+    // Concrete Aggregate
+    public class Collection<T> : IteratorAggregate<T>
+    {
+        private List<T> collection = new List<T>();
+
+
+        public void Add(T item) { collection.Add(item); }
+
+        public T Get(int index) { return collection[index]; }
+
+        public int Size() { return collection.Count; }
+        
+        public override Iterator<T> CreateIterator() { return new CollectionIterator<T>(this); }
+    }
+}
--- CSC360-SB/main.cs
+++ CSC360-SB/main.cs
@@ -17,6 +17,30 @@ namespace CSC360_SB
             Console.WriteLine("> Singleton #1 = Singleton #2: " + (singleton1 == singleton2).ToString());
 
 
+            // -- BEHAVIORAL Design Pattern --
+            Console.WriteLine("\n>> BEHAVIORAL Design Pattern  (Iterator)");
+
+            Collection<int> intCollection = new Collection<int>();
+            Iterator<int> ICIterator = intCollection.CreateIterator();
+
+            intCollection.Add(1);
+            intCollection.Add(20);
+            intCollection.Add(300);
+
+            Console.WriteLine("> Int Collection Traversal:");
+            while (ICIterator.HasNext()) Console.WriteLine(ICIterator.Next().ToString());
+
+            Collection<string> stringCollection = new Collection<string>();
+            Iterator<string> SIterator = stringCollection.CreateIterator();
+
+            stringCollection.Add("First");
+            stringCollection.Add("Second");
+            stringCollection.Add("Third");
+
+            Console.WriteLine("> String Collection Traversal:");
+            while (SIterator.HasNext()) Console.WriteLine(SIterator.Next().ToString());
+
+
             // -- STRUCTURAL Design Pattern
             Console.WriteLine("\n>> STRUCTURAL Design Pattern  (Facade)");
 
